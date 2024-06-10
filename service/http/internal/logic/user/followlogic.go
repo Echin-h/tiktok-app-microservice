847945d5855dd9561cb76da326e560fd3879741d
Echin-h <23050118@hdu.edu.cn>
2024-06-10 20:13:39 +0800

    feat : followlogic.go

--- service/http/internal/logic/user/followlogic.go
+++ service/http/internal/logic/user/followlogic.go
@@ -2,6 +2,9 @@ package user
 
 import (
 	"context"
+	"tiktok-app-microservice/common/err/apiErr"
+	"tiktok-app-microservice/common/middleware"
+	"tiktok-app-microservice/service/rpc/user/types/user"
 
 	"tiktok-app-microservice/service/http/internal/svc"
 	"tiktok-app-microservice/service/http/internal/types"
@@ -24,7 +27,70 @@ func NewFollowLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FollowLogi
 }
 
 func (l *FollowLogic) Follow(req *types.FollowReq) (resp *types.FollowResp, err error) {
-	// todo: add your logic here and delete this line
+	l.WithContext(l.ctx).Infof("关注用户: %v", req)
 
-	return
+	var id int64
+	id, err = middleware.GetUserIdFromToken(req.Token, l.svcCtx.Config.Auth.AccessSecret)
+	if err != nil {
+		return nil, apiErr.InvalidToken
+	}
+
+	if id == req.ToUserId {
+		return nil, apiErr.IllegalOperation.WithDetails("不能关注自己")
+	}
+
+	if req.ActionType == 1 {
+		// 查看是否已经关注
+		isFollowReply, _ := l.svcCtx.UserRpc.IsFollow(l.ctx, &user.IsFollowRequest{
+			UserId:       id,
+			FollowUserId: req.ToUserId,
+		})
+		if isFollowReply.IsFollow {
+			logx.WithContext(l.ctx).Errorf("已经关注过了")
+			return nil, apiErr.AlreadyFollowed
+		}
+
+		// 关注
+		_, err := l.svcCtx.UserRpc.FollowUser(l.ctx, &user.FollowUserRequest{
+			UserId:       id,
+			FollowUserId: req.ToUserId,
+		})
+
+		if err != nil {
+			logx.WithContext(l.ctx).Errorf("关注失败: %v", err)
+			return nil, apiErr.InternalError(l.ctx, err.Error())
+		}
+
+		// TODO: 异步任务添加好友关系
+	} else if req.ActionType == 0 {
+		// 查看是否关注
+		isFollowReply, _ := l.svcCtx.UserRpc.IsFollow(l.ctx, &user.IsFollowRequest{
+			UserId:       id,
+			FollowUserId: req.ToUserId,
+		})
+
+		if !isFollowReply.IsFollow {
+			logx.WithContext(l.ctx).Errorf("还未关注")
+			return nil, apiErr.NotFollowed
+		}
+
+		// 取消关注
+		_, err := l.svcCtx.UserRpc.UnFollowUser(l.ctx, &user.UnFollowUserRequest{
+			UserId:         id,
+			UnFollowUserId: req.ToUserId,
+		})
+
+		if err != nil {
+			logx.WithContext(l.ctx).Errorf("取消关注失败: %v", err)
+			return nil, apiErr.InternalError(l.ctx, err.Error())
+		}
+
+		// TODO: 异步任务解除好友关系
+	} else {
+		logx.WithContext(l.ctx).Errorf("ActionType参数错误")
+		return nil, apiErr.InvalidParams.WithDetails("ActionType参数错误")
+	}
+	return &types.FollowResp{
+		BasicReply: types.BasicReply(apiErr.Success),
+	}, nil
 }
--- service/rpc/user/internal/logic/followuserlogic.go
+++ service/rpc/user/internal/logic/followuserlogic.go
@@ -2,6 +2,12 @@ package logic
 
 import (
 	"context"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"tiktok-app-microservice/common/err/rpcErr"
+	"tiktok-app-microservice/common/model"
+	"tiktok-app-microservice/common/utils"
 
 	"tiktok-app-microservice/service/rpc/user/internal/svc"
 	"tiktok-app-microservice/service/rpc/user/types/user"
@@ -24,7 +30,55 @@ func NewFollowUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Follow
 }
 
 func (l *FollowUserLogic) FollowUser(in *user.FollowUserRequest) (*user.Empty, error) {
-	// todo: add your logic here and delete this line
+	l.WithContext(l.ctx).Infof("关注用户: %v", in)
 
+	err := l.svcCtx.DBList.Mysql.Transaction(func(tx *gorm.DB) error {
+		var u *model.User
+		var followUser *model.User
+		tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where("id = ?", in.UserId).First(u)
+		tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where("id = ?", in.FollowUserId).First(followUser)
+
+		// 1. 处理关注用户
+		result, err := l.svcCtx.DBList.Redis.Exists(l.ctx, utils.GenUserInfoCacheKey(in.UserId)).Result()
+		if err != nil {
+			l.Logger.Error(rpcErr.CacheError.Code, err.Error())
+		}
+		if result == 1 {
+			// TODO: 如果是大V (在redis中有缓存), 就只更新缓存, 交给定时任务更新数据库
+		} else {
+			// 如果是普通用户, 就直接更新数据库
+			u.FollowCount++
+			err := tx.Clauses(clause.OnConflict{UpdateAll: true}).Update("follow_count", u.FollowCount).Error
+			if err != nil {
+				l.WithContext(l.ctx).Errorf("更新普通用户关注数失败: %v", err)
+				return status.Error(rpcErr.DataBaseError.Code, err.Error())
+			}
+		}
+
+		// 2. 处理被关注用户
+		result, err = l.svcCtx.DBList.Redis.Exists(l.ctx, utils.GenUserInfoCacheKey(in.FollowUserId)).Result()
+		if err != nil {
+			l.Logger.Error(rpcErr.CacheError.Code, err.Error())
+		}
+		if result == 1 {
+			// TODO: 如果是大V (在redis中有缓存), 就只更新缓存, 交给定时任务更新数据库
+		} else {
+			followUser.FanCount++
+			err := tx.Clauses(clause.OnConflict{UpdateAll: true}).Update("fan_count", followUser.FanCount).Error
+			if err != nil {
+				return status.Error(rpcErr.DataBaseError.Code, err.Error())
+			}
+		}
+
+		err = tx.Clauses(clause.Locking{Strength: "UPDATE"}).Model(u).Association("Follows").Append(followUser)
+		if err != nil {
+			return status.Error(rpcErr.DataBaseError.Code, err.Error())
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(rpcErr.DataBaseError.Code, err.Error())
+	}
 	return &user.Empty{}, nil
 }
